e5af0db55d908ce05cd74d0ec2a6186a881d1478
Abhijit <aby.tml@gmail.com>
2017-10-31 21:34:37 +0000

    Update scrapper.java
    added support for uptime robot

--- src/scrapper.java
+++ src/scrapper.java
@@ -6,6 +6,8 @@ import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.auth.AnonymousAuthenticationHandler;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.squareup.okhttp.*;
+import org.codehaus.jettison.json.JSONObject;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.BatchPoints;
@@ -34,8 +36,11 @@ public class scrapper {
             SearchRestClient searchClient = restClient.getSearchClient();
 
 
-            SearchResult result = searchClient.searchJql("reporter='Richard Millet' and status in(Open)").claim();
-            Integer openissue = result.getTotal();
+            SearchResult resultopen = searchClient.searchJql("reporter='Richard Millet' and status in(Open)").claim();
+            Integer openissue = resultopen.getTotal();
+
+            SearchResult resultclose = searchClient.searchJql("reporter='Richard Millet' and status in(Closed)").claim();
+            Integer closeissue = resultclose.getTotal();
 
             //influxdb connection
             String dbName = "jira";
@@ -51,23 +56,57 @@ public class scrapper {
             Point point1 = Point.measurement("open")
                     .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
                     .addField("OpenIssue", openissue)
-                    .tag("status", "issuecount") // tag the individual point
+                    .build();
+
+            Point point2 = Point.measurement("close")
+                    .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                    .addField("ClosedIssue", closeissue)
                     .build();
 
             batchPoints.point(point1);
+            batchPoints.point(point2);
 
             influxDB.write(batchPoints);
 
-            Query query = new Query("SELECT * FROM open", dbName);
+
+            //Uptime Robot Client
+            OkHttpClient client = new OkHttpClient();
+
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            RequestBody body = RequestBody.create(mediaType, "api_key=u508431-1dbdee24b5c1dac6f4d16322&format=json&logs=1");
+            Request request = new Request.Builder()
+                    .url("https://api.uptimerobot.com/v2/getMonitors")
+                    .post(body)
+                    .addHeader("content-type", "application/x-www-form-urlencoded")
+                    .addHeader("cache-control", "no-cache")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            int i = response.toString().indexOf("{");
+            String str = response.toString().substring(i);
+            str = str.replaceAll("/", "");
+            str = str.replaceAll(":", "");
+            JSONObject obj = new JSONObject(str.trim());
+            System.out.println("Uptime: " +obj.getString("message"));
+
+         //InfluxDb Write
+            Point point3 = Point.measurement("uptime")
+                    .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                    .addField("msg", obj.getString("message"))
+                    .build();
+
+            batchPoints.point(point3);
+
+            influxDB.write(batchPoints);
+
+
+            Query query = new Query("SELECT * FROM uptime", dbName);
             QueryResult queryResult = influxDB.query(query);
 
             for (QueryResult.Result count : queryResult.getResults()) {
 
-                System.out.println("Open Issues: " +count.toString());
+                System.out.println("Uptime Trend: " +count.toString());
             }
-
-
-
 
         }catch(Exception e){
             e.printStackTrace();
